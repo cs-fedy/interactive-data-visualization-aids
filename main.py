e743319fb4efd4903407c5a5326ab76ef5ecf701
cs-fedy <fedi.abd001@gmail.com>
2022-12-02 05:17:27 +0100

    done

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ def get_regions(df: pd.DataFrame):
 
 
 def select_region_rows(df: pd.DataFrame, region: str) -> pd.DataFrame:
-    if region == "All":
+    if region == "Touts":
         return df
     return df[df["regions"] == region]
 
@@ -42,7 +42,7 @@ def handle_bar_chart(plot_details: PlotDetails):
     df = select_region_rows(plot_details.df, plot_details.selected_region)
     fig = px.bar(
         df,
-        x="regions" if plot_details.selected_region == "All" else "countries",
+        x="regions" if plot_details.selected_region == "Touts" else "countries",
         y="_".join(plot_details.selected_age.split(" ")),
         title=plot_details.df_title,
     )
@@ -54,7 +54,7 @@ def handle_pie_chart(plot_details: PlotDetails):
     fig = px.pie(
         df,
         values="_".join(plot_details.selected_age.split(" ")),
-        names="regions" if plot_details.selected_region == "All" else "countries",
+        names="regions" if plot_details.selected_region == "Touts" else "countries",
         title=plot_details.df_title,
         width=700,
         height=700,
@@ -73,7 +73,7 @@ def handle_click(plot_details: PlotDetails):
 
 
 def get_quote():
-    with open(r"quotes.en.fr.txt", mode="r") as file:
+    with open(r"quotes.txt", mode="r") as file:
         quotes = file.readlines()
     return choice(quotes)
 
@@ -81,52 +81,32 @@ def get_quote():
 hiv_incidence_df, aids_mortality_df, children_living_hiv_df = get_data()
 
 regions = list(get_regions(hiv_incidence_df))
-regions.insert(0, "All")
+regions.insert(0, "Touts")
 
 type_to_dataset = {
-    "hiv incidence per 1000 uninfected population": hiv_incidence_df,
-    "AIDS-related mortality per 100,000 population": aids_mortality_df,
-    "Number of children living with HIV": children_living_hiv_df,
+    "Incidence du VIH pour 1000 habitants non infectés": hiv_incidence_df,
+    "Mortalité liée au SIDA pour 100 000 habitants": aids_mortality_df,
+    "Nombre d'enfants vivant avec le VIH": children_living_hiv_df,
 }
 
 
-def add_logo():
-    st.markdown(
-        """
-        <style>
-            [data-testid="stSidebarNav"] {
-                background-image: url(https://w7.pngwing.com/pngs/962/58/png-transparent-logo-font-design-ribbon-logo-world-aids-day-thumbnail.png);
-                background-repeat: no-repeat;
-                padding-top: 120px;
-                background-position: 20px 20px;
-            }
-            [data-testid="stSidebarNav"]::before {
-                content: "My Company Name";
-                margin-left: 20px;
-                margin-top: 20px;
-                font-size: 30px;
-                position: relative;
-                top: 100px;
-            }
-        </style>
-        """,
-        unsafe_allow_html=True,
-    )
-
-
-st.title("🎀 Interactive data visualization about  HIV/AIDS: EPIDEMIOLOGY")
+st.title("🎀 Visualisation interactive de données sur le VIH/SIDA : ÉPIDÉMIOLOGIE")
+st.markdown(
+    "[Source de données <<UNICEF_State_of_World_Children_2021>>](https://www.kaggle.com/datasets/nguyenngocphung/unicef-state-of-world-children-2021?resource=download)"
+)
 with st.sidebar:
-    add_logo()
-    donneSelected = st.selectbox("donnes", type_to_dataset.keys())
-    ageSelected = st.selectbox("Ages", ["children 0-14", "girls 10–19", "boys 10–19"])
-    regionSelected = st.selectbox("Pick a region", regions)
-    st.header("_**Quotes of the day**_")
-    st.markdown(get_quote())
+    st.title("🎀 Protégez-vous du SIDA")
+
+    donneSelected = st.selectbox("Types", type_to_dataset.keys())
+    ageSelected = st.selectbox("Âge", ["children 0-14", "girls 10–19", "boys 10–19"])
+    regionSelected = st.selectbox("Régions", regions)
+    st.header("_**Citations du jour**_")
+    st.caption(get_quote())
 
 plot_details = PlotDetails(
     type_to_dataset[donneSelected],
     donneSelected,
-    "bar" if donneSelected == "Number of children living with HIV" else "pie",
+    "bar" if donneSelected == "Nombre d'enfants vivant avec le VIH" else "pie",
     regionSelected,
     ageSelected,
 )
